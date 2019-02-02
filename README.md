1b89bf8a38e052ff0e0ccb5057ae9649ff96a0f6
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-02-01 20:05:22 -0800

    created firebase

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Stephanie Expense-Tracker 
 Master Branch for final project
-
-
-// this is a test for my frist approve ///
-
-hello ?
---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from "firebase";
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAHEz-HgcH4mhme2bAPFaK7YUMHcLaLRuk",
+    authDomain: "queenbelladrama.firebaseapp.com",
+    databaseURL: "https://queenbelladrama.firebaseio.com",
+    projectId: "queenbelladrama",
+    storageBucket: "queenbelladrama.appspot.com",
+    messagingSenderId: "62006215142"
+  };
+  firebase.initializeApp(config);
+
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+
+
+
+
+
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export { auth, db };

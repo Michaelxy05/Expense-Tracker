f24a4ed002b0d797336c78a498cd3e7f75422edd
michaelxy05 <michaelxy05@gmail.com>
2019-02-09 23:47:51 -0800

    Add space

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -7,7 +7,7 @@ class Card extends React.Component {
         return (
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{this.props.title}</h5>
+                    <h5 className="card-title"> {this.props.title}</h5>
                     <h6 className="card-subtitle">{this.props.subtitle}</h6>
                     <p className="card-text">{this.props.text}.</p>
                 </div>
